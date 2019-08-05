0001a54f6a9933f213338f5ce2c01b0690d4c32f
joethorley <joe@poissonconsulting.ca>
2019-08-05 16:30:31 -0700

    batch_process returns flag

--- R/process.R
+++ R/process.R
@@ -3,10 +3,13 @@
 #' Performs batch processing of files in a directory using the 
 #' \code{\link{batch_config}()}, \code{\link{batch_run}()}
 #' and \code{\link{batch_cleanup}()} functions. 
+#' For more control and interactivity the user should call these functions
+#' instead of \code{\link{batch_process}()}.
 #' 
 #' As well as the directory and a regular expression to identify files the
 #' user must provide the function that will be used to process each file.
-#' Processing is started (and can be manually restarted) using \code{\link{batch_run}()}
+#' Processing is started (and can be manually restarted) 
+#' using \code{\link{batch_run}()}
 #' which locks the configuration file with \code{\link[filelock]{lock}} 
 #' to prevent concurrent calls. 
 #' The files that have not yet been processed can be got 
@@ -17,47 +20,41 @@
 #' Configuration is only possible if the directory does not already contain
 #' a configuration file. If \code{recursive = TRUE} then the subdirectories
 #' must also not contain configuration files.
-#' Existing configuration files can be listed using \code{\link{batch_config_files}()}.
+#' Existing configuration files can be listed 
+#' using \code{\link{batch_config_files}()}.
 #' 
 #' The pattern must match at least one non-hidden file in the directory 
 #' (or if \code{recursive = TRUE} in the directory or subdirectories).
 #' Hidden files are excluded to prevent accidental modification of system files.
 #' 
-#' The FUN function's first argument should be a string of the path to a single file.
-#' The function should return TRUE if processing was successful or FALSE
-#' if processing was unsuccessful. 
-#' If the function returns FALSE the failure is automatically logged as an INFO 
-#' message in the '.batchr.log' file.
+#' The FUN function's first argument should be 
+#' a string of the path to a single file.
+#' The function should return anything other than a FALSE if 
+#' processing was successful.
 #' 
-#' Processing is also considered to have failed if 
-#' the function throws an error. 
-#' In this case the error is caught, the failure is logged as an ERROR message 
-#' and batch processing resumes at the next file.
-#' If, however, the function returns an object other than 
-#' a flag then a FATAL message is logged and processing 
-#' ceases immediately with batchr throwing an error.
+#' Processing is only considered to have failed if the FUN returns FALSE or
+#' throws an error. 
+#' Any errors are caught and batch processing resumes at the next file.
 #' 
-#' All failures are logged in the '.batchr.log' file with the level (WARN, ERROR, FATAL),
+#' All attempts to process a file are logged in the '.batchr.log' file 
+#' with the level (INFO (success), WARN (returned FALSE), or ERROR (threw an error)),
 #' system time in UTC and file name using a \code{\link[log4r]{logger}} object.
-#' This information parsed as a tibble 
-#' with level, time and file columns using \code{\link{batch_read_log}()}.
+#' This information can be read as a tibble
+#' using \code{\link{batch_read_log}()}.
 #' 
-#' If using \code{\link{batch_run}()}, the user can log custom messages 
-#' by providing a \code{\link[log4r]{logger}} 
-#' object which is passed to the FUN function as the \code{logger} argument.
-#'
 #' @inheritParams batch_config
 #' @inheritParams batch_run
+#' @inheritParams batch_cleanup
 #'
 #' @seealso \code{\link{batch_config}()}, \code{\link{batch_run}()}
 #' and \code{\link{batch_cleanup}()}
-#' @return A flag specifying whether all files were processed successfully.
+#' @return An invisible flag indicating whether all the files where 
+#' successfully processed.
 #' @export
 batch_process <- function(FUN, path = ".", pattern = ".*", recursive = FALSE, 
-                          parallel = FALSE, ...) {
+                          parallel = FALSE, force = TRUE, ...) {
   batch_config(FUN, path = path, pattern = pattern, recursive = recursive, ...)
-  batch_run(path = path, parallel = parallel)
- # if(length(batch_remaining_files(failed = NA))) return(FALSE)
-#    batch_cleanup(path)
-  TRUE
+  success <- batch_run(path = path, parallel = parallel)
+  batch_cleanup(path, force = force)
+  all(success)
 }
--- tests/testthat/test-process.R
+++ tests/testthat/test-process.R
@@ -30,11 +30,30 @@ test_that("batch_process changes files", {
     TRUE
   }
 
-  batch_process(FUN, path)
+  expect_true(batch_process(FUN, path))
   # should be x = 2L
   expect_identical(read.csv(file.path(path, "file1.csv")), 
                    structure(list(X.1 = 1L, X = 1L, x = 2L), class = "data.frame", row.names = c(NA, -1L)))
   # should be x = 6L
   expect_identical(read.csv(file.path(path, "file2.csv")), 
                    structure(list(X.1 = 1L, X = 1L, x = 6L), class = "data.frame", row.names = c(NA, -1L)))
+})
+
+test_that("batch_process with failure", {
+  teardown(unlink(file.path(tempdir(), "batchr_process")))
+  
+  path <- file.path(tempdir(), "batchr_process")
+  unlink(path, recursive = TRUE)
+  dir.create(path)
+  
+  write.csv(data.frame(x = 1), file.path(path, "file1.csv"))
+  write.csv(data.frame(x = 3), file.path(path, "file2.csv"))
+  
+  FUN <- function(file) grepl(file, "file1[.]csv$")
+
+  expect_error(batch_read_config(path),
+               "^Can't find file '.*[.]batchr.rds'[.]$")
+  expect_false(batch_process(FUN, path))
+  expect_error(batch_read_config(path),
+               "^Can't find file '.*[.]batchr.rds'[.]$")
 })
