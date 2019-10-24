7d66e3933e13c3938b48b6e6f4ec28887032df76
joethorley <joe@poissonconsulting.ca>
2019-10-24 07:06:45 -0700

    rename files from summary to report

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -17,9 +17,9 @@ test_that("batch_report all processed successfully", {
   skip_on_cran()
   skip_on_travis()
   skip_on_appveyor()
-  verify_output(test_path("output/summary1.txt"), batch_report(path))
+  verify_output(test_path("output/report1.txt"), batch_report(path))
 
   expect_identical(batch_run(path, ask = FALSE), c(file1.csv = TRUE))
 
-  verify_output(test_path("output/summary2.txt"), batch_report(path))
+  verify_output(test_path("output/report2.txt"), batch_report(path))
 })
