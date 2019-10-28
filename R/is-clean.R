52b60a9a39ed6f5ad689d915a8f953b2ea2cc060
joethorley <joe@poissonconsulting.ca>
2019-10-28 15:31:50 -0700

    added batch_is_clean() to test if clean

---
+++ R/is-clean.R
@@ -0,0 +1,14 @@
+#' Is Clean
+#'
+#' Tests whether directory contains configuration file created by [batch_config()].
+#'
+#' @inheritParams batch_config
+#' @inheritParams batch_run
+#' @seealso [batch_cleanup()]
+#' @export
+batch_is_clean <- function(path = ".", recurse = FALSE) {
+  chk_dir(path)
+  chk_flag(recurse)
+  
+  !length(config_files(path, recursive = recurse))
+}
