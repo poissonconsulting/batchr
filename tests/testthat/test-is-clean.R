901e94555c6bfd78fe0b98ebc917c210ce10f263
Joe Thorley <joe@poissonconsulting.ca>
2021-01-27 09:53:41 -0800

    yet more local_tempdir()

--- tests/testthat/test-is-clean.R
+++ tests/testthat/test-is-clean.R
@@ -1,12 +1,8 @@
 context("is-clean")
 
 test_that("batch_is_clean()", {
-  teardown(unlink(file.path(tempdir(), "batchr_start")))
-
-  path <- file.path(tempdir(), "batchr_start")
-  unlink(path, recursive = TRUE)
-  dir.create(path)
-
+  path <- withr::local_tempdir()
+  
   write.csv(data.frame(x = 1), file.path(path, "file1.csv"))
 
   expect_true(batch_is_clean(path))
@@ -28,31 +24,28 @@ test_that("batch_is_clean()", {
 })
 
 test_that("batch_is_clean() recurse", {
-  teardown(unlink(file.path(tempdir(), "batchr_start")))
+  path <- withr::local_tempdir()
+  sub <- withr::local_tempdir(tmpdir = path)
 
-  path <- file.path(tempdir(), "batchr_start/sub")
-  unlink(path, recursive = TRUE)
-  dir.create(path)
+  write.csv(data.frame(x = 1), file.path(sub, "file1.csv"))
 
-  write.csv(data.frame(x = 1), file.path(path, "file1.csv"))
-
-  expect_true(batch_is_clean(path))
-  expect_true(batch_is_clean(dirname(path)))
-  expect_true(batch_is_clean(dirname(path), recurse = TRUE))
+  expect_true(batch_is_clean(sub))
+  expect_true(batch_is_clean(dirname(sub)))
+  expect_true(batch_is_clean(dirname(sub), recurse = TRUE))
 
   expect_identical(
     batch_config(function(x) TRUE,
-      path = path,
+      path = sub,
       regexp = "^file\\d[.]csv$"
     ),
     "file1.csv"
   )
-  expect_false(batch_is_clean(path))
-  expect_true(batch_is_clean(dirname(path)))
-  expect_false(batch_is_clean(dirname(path), recurse = TRUE))
+  expect_false(batch_is_clean(sub))
+  expect_true(batch_is_clean(dirname(sub)))
+  expect_false(batch_is_clean(dirname(sub), recurse = TRUE))
 
-  expect_identical(batch_cleanup(path, force = TRUE), c(. = TRUE))
-  expect_true(batch_is_clean(path))
-  expect_true(batch_is_clean(dirname(path)))
-  expect_true(batch_is_clean(dirname(path), recurse = TRUE))
+  expect_identical(batch_cleanup(sub, force = TRUE), c(. = TRUE))
+  expect_true(batch_is_clean(sub))
+  expect_true(batch_is_clean(dirname(sub)))
+  expect_true(batch_is_clean(dirname(sub), recurse = TRUE))
 })
