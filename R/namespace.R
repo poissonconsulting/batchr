c27fc84f5b992a694154712894039f1aef190dd5
Joe Thorley <joe@poissonconsulting.ca>
2021-01-27 10:34:39 -0800

    remove unused dependency purrr

--- R/namespace.R
+++ R/namespace.R
@@ -3,6 +3,5 @@
 #' @importFrom hmstimer tmr_timer tmr_stop tmr_round tmr_floor
 #' @importFrom stats runif
 #' @importFrom yesno yesno
-#' @importFrom purrr map
 #' @importFrom furrr future_map
 NULL
